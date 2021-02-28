08906fb63445e49b1b4f995b6abe21de3f8aec96
techmahendran <mahendran361999@gmail.com>
2021-02-28 16:07:55 +0530

    my first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstName = document.querySelector('#send').addEventListener('click', (e) => {
+    // document.getElementById('logo').textContent = 'hi';
+    document.getElementById('logo').style.color = 'green';
+
+});
+
