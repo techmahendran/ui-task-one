57919006446280ac1dc82eba680308dc8ed715fd
techmahendran <mahendran361999@gmail.com>
2021-04-11 08:33:56 +0530

    my change the css , html and js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const firstName = document.querySelector('#send').addEventListener('click', (e) => {
-    // document.getElementById('logo').textContent = 'hi';
+const firstName = document.querySelector('#send');
+firstName.addEventListener('click', () => {
+    document.getElementById('logo').textContent = 'Mahendran';
     document.getElementById('logo').style.color = 'green';
+    document.querySelector('#link').style.color = 'blue';
+    document.querySelector('#link').textContent = 'Fornt-end Developer'
 
+    alert('see the first page');
 });
 
