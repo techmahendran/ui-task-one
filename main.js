4cb2d756333a55d405af3699c63d36d9b02d17de
techmahendran <mahendran361999@gmail.com>
2021-05-05 12:12:58 +0530

    change html and css code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ firstName.addEventListener('click', () => {
     document.getElementById('logo').textContent = 'Mahendran';
     document.getElementById('logo').style.color = 'green';
     document.querySelector('#link').style.color = 'blue';
-    document.querySelector('#link').textContent = 'Fornt-end Developer'
+    document.querySelector('#link').textContent = 'Front-end Developer'
 
     alert('see the first page');
 });
